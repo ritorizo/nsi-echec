e699d2302265d171f916e6a9becd3cefc8c4d545
Minemew2 <leandre.mc2@gmail.com>
2021-03-14 19:38:04 +0100

    [Interface fait par Léandre]

---
+++ Interface/carre_vide.py
@@ -0,0 +1,20 @@
+import pygame
+from x_coordonnees import *
+#marge dpionte/gauche echequier : 126
+pygame.init()
+
+# Donner un nom à la fenêtre
+
+# x = 1-8, y = 1-8
+cn = pygame.image.load("imgs/cn.png")
+cb = pygame.image.load("imgs/cb.png")
+
+cn = pygame.transform.scale(cn,(31,31))
+cb = pygame.transform.scale(cb,(31,31))
+
+
+
+
+
+
+  
