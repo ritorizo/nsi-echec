9a86fdfa89a19f6bdfb8e10f0056ee405b3fde1d
Victor <castorvictor29@gmail.com>
2021-03-25 17:54:18 +0100

    ajout

--- Moteur/VerifierCoup.py
+++ Moteur/VerifierCoup.py
@@ -1,6 +1,7 @@
 echiquier = [[["TB"],[],["KN"],[],[],[],[],[]], [[],[],[],[],["QB"],[],[],[]], [[],[],[],[],[],[],[],[]], [[],[],[],[],[],[],[],[]], [[],[],[],[],[],["KB"],[],[]], [[],[],[],[],[],[],[],[]], [['PB'],[],[],[],[],[],[],[]], [[],[],[],[],[],[],[],[]], 0]
 
 def gestion_coup_valider(mouvement): # mouvement entrée sous la forme ((0,5),(1,6))
+    
     echiquier[mouvement[1][0]] = echiquier[mouvement[0][0]] # déplacement du pion avec les coordonées entré par mouvement. (sur les trois prochaines lignes)
     echiquier[mouvement[1][1]] = echiquier[mouvement[0][1]]
     echiquier[mouvement[0][0]] = [] 
