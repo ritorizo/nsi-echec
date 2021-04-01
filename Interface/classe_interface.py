a4613839e2cb69293a1d6fb03a9b10ddac1d8a0f
Minemew2 <62471693+Minemew2@users.noreply.github.com>
2021-04-01 17:56:58 +0200

    Fin de la première méthode interface

--- Interface/classe_interface.py
+++ Interface/classe_interface.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+#!/usr/bin/python
+# -*- coding: ascii -*-
 import pygame # pip install pygame
 pygame.init()
 class Interface :
@@ -23,11 +29,14 @@ class Interface :
     police_button = pygame.font.Font(None,40)
 
     # Création des textes et des boutons
-    titre = police.render("Bienvenue dans le jeu d'échecs des élèves de NSI", True, self.BLACK ) # (1369,56)
+    titre = police.render("Bienvenue dans le jeu d'echecs des eleves de NSI", True, self.BLACK ) # (1369,56)
     txt_choix_mode = police.render("Choisissez le mode de jeu :", True, self.BLACK) # (752,56)
     button_modejcr = police.render("Mode joueur contre robot ", True, self.BLUE)
     button_modejcj = police.render("Mode joueur contre joueur", True, self.BLUE)
-    txt_choix_couleur = police.render("Le robot doit-il commencer ?", True, self.BLACK)
+    txt_choix_couleur = police.render("Le robot doit-il commencer (joueur blanc) ?", True, self.BLACK)
+    button_couleur_blanc = police.render("[Oui]",True,self.BLUE)
+    button_couleur_noir = police.render("[Non]",True,self.BLUE)
+    button_valider = police.render("valider ?",True,self.BLUE)
     button_retour = police_button.render("<- Retour ",True,self.RED) 
 
     #Récupération des tailles des textes et boutons, puis calcule leur coordonnées pour les centrer
@@ -44,10 +53,19 @@ class Interface :
     co_button_jcj = ((self.taille_ecran[0]-taille_button_jcj[2])/2,taille_titre[3]*3)
 
     taille_txt2 = txt_choix_couleur.get_rect()
-    co_txt2 = ((self.taille_ecran[0]-taille_txt2[2])/2,taille_titre[3]*4)
+    co_txt2 = ((self.taille_ecran[0]-taille_txt2[2]),taille_titre[3]*4)
+
+    taille_button_blanc = button_couleur_blanc.get_rect()
+    taille_button_noir = button_couleur_noir.get_rect()
+    co_button_blanc = ((self.taille_ecran[0]-(taille_button_blanc[2]+taille_button_noir[2]))/2,taille_titre[3]*5)
+    co_button_noir = (((self.taille_ecran[0]-(taille_button_blanc[2]+taille_button_noir[2]))/2+taille_button_blanc[2]),taille_titre[3]*5)
+
+    taille_button_valider = button_valider.get_rect()
+    co_button_valider = ((self.taille_ecran[0]-taille_button_valider[2])/2,taille_titre[3]*6)
 
     taille_button_retour = button_retour.get_rect()
     co_button_retour = ((self.taille_ecran[0]-taille_button_retour[2]),self.taille_ecran[1]-taille_button_retour[3])
+
     
     # Boucle de jeu
     running = True
@@ -67,9 +85,9 @@ class Interface :
         self.etat = "rien"
 
       if self.etat == "mode jcr" and choix_mode == False:
-        print("Mode joueur contre joueur")
+        #print("Mode joueur contre joueur")
         mode = ("J","R")
-        print(mode)
+        #print(mode)
         button_modejcr = police.render("Mode joueur contre robot ", True, self.GREEN) 
         self.screen.blit(button_modejcr,co_button_jcr)
         button_modejcj = police.render("Mode joueur contre joueur", True, self.GRAY)
@@ -77,9 +95,9 @@ class Interface :
         choix_mode = True
 
       if self.etat == "mode jcj" and choix_mode == False:
-        print("Mode joueur contre joueur")
+        #print("Mode joueur contre joueur")
         mode = ("J","J")
-        print(mode)
+        #print(mode)
         button_modejcr = police.render("Mode joueur contre robot ", True, self.GRAY) 
         self.screen.blit(button_modejcr,co_button_jcr)
         button_modejcj = police.render("Mode joueur contre joueur", True, self.GREEN)
@@ -91,7 +109,34 @@ class Interface :
       
       if self.etat == "choix couleur" :
         txt_choix_couleur = police.render("Le robot doit-il commencer ?", True, self.BLACK)
+        button_couleur_blanc = police.render("[Oui]",True,self.BLUE)
+        button_couleur_noir = police.render("[Non]",True,self.BLUE)
         self.screen.blit(txt_choix_couleur,co_txt2)
+        self.screen.blit(button_couleur_blanc,co_button_blanc)
+        self.screen.blit(button_couleur_noir,co_button_noir)
+
+      if self.etat == "robot blanc" :
+        txt_choix_couleur = police.render("Le robot doit-il commencer ?", True, self.BLACK)
+        button_couleur_blanc = police.render("[Oui]",True,self.GREEN)
+        button_couleur_noir = police.render("[Non]",True,self.GRAY)
+        self.screen.blit(txt_choix_couleur,co_txt2)
+        self.screen.blit(button_couleur_blanc,co_button_blanc)
+        self.screen.blit(button_couleur_noir,co_button_noir)
+        mode = ("R","J")
+
+      if self.etat == "robot noir" :
+        txt_choix_couleur = police.render("Le robot doit-il commencer ?", True, self.BLACK)
+        button_couleur_blanc = police.render("[Oui]",True,self.GRAY)
+        button_couleur_noir = police.render("[Non]",True,self.GREEN)
+        self.screen.blit(txt_choix_couleur,co_txt2)
+        self.screen.blit(button_couleur_blanc,co_button_blanc)
+        self.screen.blit(button_couleur_noir,co_button_noir)
+        mode = ("J","R")
+
+      if self.etat == "mode jcj" or self.etat == "robot blanc" or self.etat == "robot noir" :
+        choix_choisi = True
+        self.screen.blit(button_valider,co_button_valider)
+
 
       for event in pygame.event.get():
         if event.type == pygame.QUIT : 
@@ -111,10 +156,21 @@ class Interface :
             if mouse_pos[0] >= co_button_jcj[0] and mouse_pos[0] <= co_button_jcj[0] + taille_button_jcj[2] and mouse_pos[1] >= co_button_jcj[1] and mouse_pos[1] <= co_button_jcj[1] + taille_button_jcj[3] :
               self.etat = "mode jcj"
 
+            if mouse_pos[0] >= co_button_blanc[0] and mouse_pos[0] <= co_button_blanc[0] + taille_button_blanc[2] and mouse_pos[1] >= co_button_blanc[1] and mouse_pos[1] <= co_button_blanc[1] + taille_button_blanc[3] and self.etat == "choix couleur" :
+              self.etat = "robot blanc"
+
+            if mouse_pos[0] >= co_button_noir[0] and mouse_pos[0] <= co_button_noir[0] + taille_button_noir[2] and mouse_pos[1] >= co_button_noir[1] and mouse_pos[1] <= co_button_blanc[1] + taille_button_noir[3] and self.etat == "choix couleur" :
+              self.etat = "robot noir"
+
+            if mouse_pos[0] >= co_button_valider[0] and mouse_pos[0] <= co_button_valider[0] + taille_button_valider[2] and mouse_pos[1] >= co_button_valider[1] and mouse_pos[1] <= co_button_valider[1] + taille_button_valider[3] and choix_choisi == True :
+              return mode
+              print("fin")
+              running = False
+
             if mouse_pos[0] >= co_button_retour[0] and mouse_pos[0] <= self.taille_ecran[0] and mouse_pos[1] >= co_button_retour[1] and mouse_pos[1] <= self.taille_ecran[1] :
               self.etat = "demaragge"
-              print("retour")
+              #print("retour")
           
 
 interface = Interface()
-interface.menuDemarrage()
+print(interface.menuDemarrage())
