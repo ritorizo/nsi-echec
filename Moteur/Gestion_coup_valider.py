3e41702edc6b8190eace131524f8826dfd8a136f
Victor <65656020+Chat-mots@users.noreply.github.com>
2021-04-06 09:29:10 +0200

    ajout d'un renvoi du déplacement dans la fonction get_next_piece

--- Moteur/Gestion_coup_valider.py
+++ Moteur/Gestion_coup_valider.py
@@ -4,6 +4,8 @@ echiquier = [[["TB"],[],["KN"],[],[],[],[],[]], [[],[],[],[],["QB"],[],[],[]], [
 
 def gestion_coup_valider(mouvement): # mouvement entrée sous la forme ((0,5),(1,6))
     
+    deplacement = mouvement
+
     sauvegarde(echiquier)
 
     echiquier[mouvement[1][0]] = echiquier[mouvement[0][0]] # déplacement du pion avec les coordonées entré par mouvement. (sur les trois prochaines lignes)
@@ -12,6 +14,6 @@ def gestion_coup_valider(mouvement): # mouvement entrée sous la forme ((0,5),(1
     
     echiquier[-1]+=1 # on ajoute 1 au compteur de tour.
 
-    return echiquier
+    return echiquier, deplacement
 
-gestion_coup_valider(((0,0),(0,1)))
+print(gestion_coup_valider(((0,0),(0,1))))
