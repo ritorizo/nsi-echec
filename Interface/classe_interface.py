a57ed3b980144b0d6d84d08176567866e0d51113
Minemew2 <leandre.mc2@gmail.com>
2021-04-13 12:17:23 +0200

    Ajout de la méthode afficherEchiquier sur l'interface

--- Interface/classe_interface.py
+++ Interface/classe_interface.py
@@ -7,15 +7,46 @@
 import pygame # pip install pygame
 pygame.init()
 class Interface :
-  taille_ecran = (800,400)
+  taille_ecran = (800,450)
   screen = pygame.display.set_mode(taille_ecran)
   pygame.display.set_caption("Echecs.exe")
+  marge = int((taille_ecran[0]-taille_ecran[1])/2)
+  taille_case = int(taille_ecran[1]/9)
+  
   BLACK = (0, 0, 0)
+  WHITE = (255,255,255)
   BLUE = (0,0,255)
   GRAY = (128,128,128)
   RED = (255,0,0)
   GREEN = (0,255,0)
   VIDE = (207,185,151)
+
+  img_CB = pygame.image.load("Interface\imgs\CB.png")
+  img_CB = pygame.transform.scale(img_CB,(taille_case,taille_case))
+  img_CN = pygame.image.load("Interface\imgs\CN.png")
+  img_CN = pygame.transform.scale(img_CN,(taille_case,taille_case))
+  img_FB = pygame.image.load("Interface\imgs\FB.png")
+  img_FB = pygame.transform.scale(img_FB,(taille_case,taille_case))
+  img_FN = pygame.image.load("Interface\imgs\FN.png")
+  img_FN = pygame.transform.scale(img_FN,(taille_case,taille_case))
+  img_KB = pygame.image.load("Interface\imgs\KB.png")
+  img_KB = pygame.transform.scale(img_KB,(taille_case,taille_case))
+  img_KN = pygame.image.load("Interface\imgs\KN.png")
+  img_KN = pygame.transform.scale(img_KN,(taille_case,taille_case))
+  img_PB = pygame.image.load("Interface\imgs\PB.png")
+  img_PB = pygame.transform.scale(img_PB,(taille_case,taille_case))
+  img_PN = pygame.image.load("Interface\imgs\PN.png")
+  img_PN = pygame.transform.scale(img_PN,(taille_case,taille_case))
+  img_QB = pygame.image.load("Interface\imgs\QB.png")
+  img_QB = pygame.transform.scale(img_QB,(taille_case,taille_case))
+  img_QN = pygame.image.load("Interface\imgs\QN.png")
+  img_QN = pygame.transform.scale(img_QN,(taille_case,taille_case))
+  img_TB = pygame.image.load("Interface\imgs\TB.png")
+  img_TB = pygame.transform.scale(img_TB,(taille_case,taille_case))
+  img_TN = pygame.image.load("Interface\imgs\TN.png")
+  img_TN = pygame.transform.scale(img_TN,(taille_case,taille_case))
+
+  echiquier = [[["TN"],["CN"],["FN"],["QN"],["KN"],["FN"],["CN"],["TN"]],[["PN"],["PN"],["PN"],["PN"],["PN"],["PN"],["PN"],["PN"]],[[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[]],[["PB"],["PB"],["PB"],["PB"],["PB"],["PB"],["PB"],["PB"]],[["TB"],["CB"],["FB"],["QB"],["KB"],["FB"],["CB"],["TB"]],0]
   etat = "rien"
   def menuDemarrage(self) :
 
@@ -170,7 +201,87 @@ class Interface :
             if mouse_pos[0] >= co_button_retour[0] and mouse_pos[0] <= self.taille_ecran[0] and mouse_pos[1] >= co_button_retour[1] and mouse_pos[1] <= self.taille_ecran[1] :
               self.etat = "demaragge"
               #print("retour")
+
+  def generationEchiquier(self) :
+
+    self.screen.fill(self.VIDE)
+    blanc = True
+    gene_case = False
+    gene_pion = False
+    gene_co = False
+    co_lettres = ["A","B","C","D","E","F","G","H"]
+    co_chiffres = ["1","2","3","4","5","6","7","8"]
+    police = pygame.font.Font(None,self.taille_case)
+
+    running = True
+    while running == True :
+      pygame.display.flip()
+      if gene_case == False :
+        for y in range(8) :
+          for x in range(8) :
+            if blanc == True : 
+              pygame.draw.rect(self.screen,self.WHITE,[self.marge+self.taille_case+(self.taille_case*x),self.taille_case+(self.taille_case*y),self.taille_case,self.taille_case])
+              blanc = False
+            elif blanc == False:
+              pygame.draw.rect(self.screen,self.BLACK,[self.marge+self.taille_case+(self.taille_case*x),self.taille_case+(self.taille_case*y),self.taille_case,self.taille_case])
+              blanc = True
+            if x == 7:
+              if blanc == True:
+                blanc = False
+              elif blanc == False :
+                blanc = True
+      gene_case = True
+      if gene_pion == False :
+        for y in range(8) :
+          for x in range(8) :
+            pion_candidat = self.echiquier[y][x]
+            if len(pion_candidat) >= 1 :
+              pion_candidat = pion_candidat[0]
+              if pion_candidat == "CB" :
+                self.screen.blit(self.img_CB,[self.marge+self.taille_case+(self.taille_case*x),self.taille_case+(self.taille_case*y),self.taille_case,self.taille_case])
+              if pion_candidat == "CN" :
+                self.screen.blit(self.img_CN,[self.marge+self.taille_case+(self.taille_case*x),self.taille_case+(self.taille_case*y),self.taille_case,self.taille_case])
+              if pion_candidat == "FB" :
+                self.screen.blit(self.img_FB,[self.marge+self.taille_case+(self.taille_case*x),self.taille_case+(self.taille_case*y),self.taille_case,self.taille_case])
+              if pion_candidat == "FN" :
+                self.screen.blit(self.img_FN,[self.marge+self.taille_case+(self.taille_case*x),self.taille_case+(self.taille_case*y),self.taille_case,self.taille_case])
+              if pion_candidat == "KB" :
+                self.screen.blit(self.img_KB,[self.marge+self.taille_case+(self.taille_case*x),self.taille_case+(self.taille_case*y),self.taille_case,self.taille_case])
+              if pion_candidat == "KN" :
+                self.screen.blit(self.img_KN,[self.marge+self.taille_case+(self.taille_case*x),self.taille_case+(self.taille_case*y),self.taille_case,self.taille_case])
+              if pion_candidat == "PB" :
+                self.screen.blit(self.img_PB,[self.marge+self.taille_case+(self.taille_case*x),self.taille_case+(self.taille_case*y),self.taille_case,self.taille_case])
+              if pion_candidat == "PN" :
+                self.screen.blit(self.img_PN,[self.marge+self.taille_case+(self.taille_case*x),self.taille_case+(self.taille_case*y),self.taille_case,self.taille_case])
+              if pion_candidat == "QB" :
+                self.screen.blit(self.img_QB,[self.marge+self.taille_case+(self.taille_case*x),self.taille_case+(self.taille_case*y),self.taille_case,self.taille_case])
+              if pion_candidat == "QN" :
+                self.screen.blit(self.img_QN,[self.marge+self.taille_case+(self.taille_case*x),self.taille_case+(self.taille_case*y),self.taille_case,self.taille_case])
+              if pion_candidat == "TB" :
+                self.screen.blit(self.img_TB,[self.marge+self.taille_case+(self.taille_case*x),self.taille_case+(self.taille_case*y),self.taille_case,self.taille_case])
+              if pion_candidat == "TN" :
+                self.screen.blit(self.img_TN,[self.marge+self.taille_case+(self.taille_case*x),self.taille_case+(self.taille_case*y),self.taille_case,self.taille_case])
+      gene_pion = True
+      if gene_co == False :
+        for x in range(8) :
+          co = police.render(co_chiffres[x], True, self.BLACK )
+          self.screen.blit(co,(self.marge,(self.taille_ecran[1]-(self.taille_case+(self.taille_case*x))+10)))
+        for x in range(8) :
+          co = police.render(co_lettres[x],True,self.BLACK)
+          self.screen.blit(co,(((self.marge+self.taille_case)+self.taille_case*x+10),10))
+      gene_co = False
+
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT : 
+          running = False
+          pygame.quit()
+          print("Fermeture")
+
+    
+
+
           
 
 interface = Interface()
-print(interface.menuDemarrage())
+#print(interface.menuDemarrage())
+interface.generationEchiquier()
